e47778c8fe9654afdaffd34934fd88d77d2ef5d4
tarasmelko <tarasxmelko@gmail.com>
2014-10-22 12:09:07 +0300

    add new files

---
+++ Assets/Scripts/BuildRotate.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildRotate : MonoBehaviour {
+
+	public GameObject player;                      // Reference to the player.
+
+	private bool rotate = false;
+
+	void OnTriggerEnter (Collider other)
+	{
+		if(other.gameObject == player)
+		{
+			this.gameObject.transform.Rotate (0, 0, 1);
+		}
+	}
+	
+}
---
+++ Assets/Scripts/KeyGrabScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyGrabScript : MonoBehaviour {
+
+	public GameObject player;                      // Reference to the player.
+     		
+	void OnTriggerEnter (Collider other)
+	{
+		if(other.gameObject == player)
+		{
+			Destroy(gameObject);
+		}
+	}
+
+}
